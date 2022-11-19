dbe37e60e938d3e42b6d27b4991dfa4cc0e21816
Rahul M <mrahulmurali30@gmail.com>
2022-11-19 14:43:48 +0530

    Final

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -1,35 +1,33 @@
 import numpy as np
 import os
-from tensorflow import keras
-
-from keras import models
-from keras.models import load_model
-from keras.preprocessing import image
-from keras.applications.inception_v3 import preprocess_input
-import requests
-from flask import Flask, request, render_template, redirect, url_for
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from flask import Flask, request,flash, render_template, redirect,url_for
 from cloudant.client import Cloudant
-model = load_model(r".\model\Updated-Xception-diabetic-retinopathy.h5")
+from twilio.rest import Client
+model = load_model(r"Updated-xception-diabetic-retinopathy.h5")
 app = Flask(__name__)
+app.secret_key="abc"
+app.config['UPLOAD_FOLDER'] = "User_Images"
 # Authenticate using an IAM API key
-client = Cloudant.iam('53fa1373-d494-4645-b98e-15660d5d1371-bluemix',
-                        'w65pj7RR76K5K8r_r86w0fSbKsk03BNAU83IOyUzR6Eg', connect=True)
+client = Cloudant.iam('5e183e68-6288-4c71-84fb-5ce6fe11e2a7-bluemix','rn2GnKgXfP0v_qWogmV-MqgMlqkxeWHD9MTbjWdPWCJ1',connect=True)
 # Create a database using an initialized client
-my_database = client.create_database('my_db')
+my_database = client.create_database('my_database')
 if my_database.exists():
     print("Database '{0}' successfully created.".format('my_db'))
 # default home page or route
+
+user = ""
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pred="Login", vis ="visible")
 
 @ app.route('/index')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", pred="Login", vis ="visible")
 
-'''@ app.route('/register')
-def register():
-    return render_template("register.html")'''
 
 # registration page
 @ app.route('/register',methods=["GET","POST"])
@@ -57,45 +55,43 @@ def register():
             return render_template('register.html', pred=" You are already a member , please login using your details ")
     else:
         return render_template('register.html')
-    
+
 
 @ app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == "POST":
-        user = request.form.get('name')
-        passw = request.form.get('pass')
+    if request.method == "GET":
+        user = request.args.get('mail')
+        passw = request.args.get('pass')
         print(user, passw)
-        query = {'_id': {'$eq': user}}
+        query = {'mail': {'$eq': user}}
         docs = my_database.get_query_result(query)
         print(docs)
         print(len(docs.all()))
         if (len(docs.all()) == 0):
-            return render_template('login.html', pred="The username is not found.")
+            return render_template('login.html', pred="")
         else:
-            if ((user == docs[0][0]['_id'] and passw == docs[0][0]['pswd'])):
-                return redirect(url_for('prediction'))
+            if ((user == docs[0][0]['mail'] and passw == docs[0][0]['psw'])):
+                flash("Logged in as " + str(user))
+                return render_template('index.html', pred="Logged in as "+str(user), vis ="hidden", vis2="visible")
             else:
-                print('Invalid User')
+                return render_template('login.html', pred="The password is wrong.")
     else:
         return render_template('login.html')
+
 
 @ app.route('/logout')
 def logout():
     return render_template('logout.html')
 
-@app.route("/predict")
+@app.route("/predict",methods=["GET", "POST"])
 def predict():
-    return render_template("prediction.html")
-
-@ app.route('/result', methods=["GET", "POST"])
-def res():
     if request.method == "POST":
-        f = request.files['image']
+        f = request.files['file']
         # getting the current path 1.e where app.py is present
         basepath = os.path.dirname(__file__)
         #print ( " current path " , basepath )
         # from anywhere in the system we can give image but we want that
-        filepath = os.path.join(basepath, 'uploads', f.filename)
+        filepath = os.path.join(str(basepath), 'User_Images', str(f.filename))
         #print ( " upload folder is " , filepath )
         f.save(filepath)
         img = image.load_img(filepath, target_size=(299, 299))
@@ -104,14 +100,14 @@ def res():
         #print ( x )
         img_data = preprocess_input(x)
         prediction = np.argmax(model.predict(img_data), axis=1)
-        # prediction = model.predict ( x ) #instead of predict_classes ( x ) we can use predict ( X ) ---- > predict_classes ( x ) gave error
-        # print ( " prediction is prediction )
-        index = [' No Diabetic Retinopathy ', ' Mild DR ',
-                 ' Moderate DR ', ' Severe DR ', ' Proliferative DR ']
-        # result = str ( index [ output [ 011 )
+        index = [' No Diabetic Retinopathy ', ' Mild NPDR ',
+                 ' Moderate NPDR ', ' Severe NPDR ', ' Proliferative DR ']
         result = str(index[prediction[0]])
         print(result)
-        return render_template('prediction.html', prediction=result)
+        return render_template('prediction.html', prediction=result, fname = filepath)
+    else:
+        return render_template("prediction.html")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.debug = False
+    app.run()
